4cb2d2e5552d356681505b981cf90f8fb8ec080a
Tristan Yang <tunkshif@foxmail.com>
2026-03-27 21:26:44 +0800

    feat: add dashboard group edit route

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,8 @@ export default [
   index("routes/_index.tsx"),
   route("login", "routes/login/route.tsx"),
   route("logout", "routes/logout.tsx"),
-  layout("routes/layout.tsx", [route("dashboard", "routes/dashboard/route.tsx")]),
+  layout("routes/layout.tsx", [
+    route("dashboard", "routes/dashboard/route.tsx"),
+    route("dashboard/group/:groupId/edit", "routes/dashboard/group/edit/route.tsx"),
+  ]),
 ] satisfies RouteConfig;
